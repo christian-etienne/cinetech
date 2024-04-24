924edde4ae0ab844d062758e3276d93d6d07550c
Pawel Barc <pawel.barc@laplateforme.io>
2024-04-24 23:04:09 +0200

    J'ai ajouté le pagination  et affichage selon la categorie

--- serie.js
+++ serie.js
@@ -1,128 +1,146 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    let currentSeriesPage = 1;
+    const itemsPerPage = 4; 
+    
+    async function fetchPage(category, page) {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTVjMzIyNTJlZWMzOGIzZTgyNDEwNTI5ZjE2NmE4NyIsInN1YiI6IjY2MjYyOTA1YjlhMGJkMDBjZGQ0MzFlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2Qpy9FgtaQ7Px6SwG910GW8KC7_eiQYMPj8vhp0laPQ'
+            }
+        };
 
-              
-              document.addEventListener('DOMContentLoaded', function() {
-                let currentSeriesPage = 1; // Aktualna strona dla seriali
-                const itemsPerPage = 8; // Liczba elementów na stronie
-            
-                function fetchMultiplePages(pagesToFetch) {
-                    const requests = [];
-                    for (let i = 1; i <= pagesToFetch; i++) {
-                        requests.push(fetchPage(i));
-                    }
-                    return Promise.all(requests);
-                }
-            
-                function fetchPage(page) {
-                    const url = `https://api.themoviedb.org/3/tv/on_the_air?language=en-US&page=${page}`;
-                    return fetch(url, {
-                        method: 'GET',
-                        headers: {
-                            accept: 'application/json',
-                            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTVjMzIyNTJlZWMzOGIzZTgyNDEwNTI5ZjE2NmE4NyIsInN1YiI6IjY2MjYyOTA1YjlhMGJkMDBjZGQ0MzFlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2Qpy9FgtaQ7Px6SwG910GW8KC7_eiQYMPj8vhp0laPQ'
-                        }
-                    })
-                    .then(response => response.json());
-                }
-            
-                function fetchProcessContent(page) {
-                    const pagesToFetch = Math.ceil(itemsPerPage / 2);
+        let url = '';
+        switch (category) {
+          
+            case 'comedy':
+                url = `https://api.themoviedb.org/3/discover/tv?with_genres=35&page=${page}`;
+                break;
+            case 'action_adventure':
+                url = `https://api.themoviedb.org/3/discover/tv?with_genres=10759&page=${page}`;
+                break;
+            case 'crime':
+                url = `https://api.themoviedb.org/3/discover/tv?with_genres=80&page=${page}`;
+                break;
+            case 'family':
+                url = `https://api.themoviedb.org/3/discover/tv?with_genres=10751&page=${page}`;
+                break;
+            default:
+                console.error('Unknown category');
+                return;
+        }
 
-                    fetchMultiplePages(pagesToFetch)
-                        .then(responses => {
-                            const allResults = responses.flatMap(response => response.results);
-                            processContent(allResults);
-                        })
-                        .catch(err => console.error(err));
-                }
+        try {
+            const response = await fetch(url, options);
+            const data = await response.json();
+
+            processTrendingSeries(data, category);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    function processTrendingSeries(data, category) {
+        const series = data.results || [];
+
+        displaySeries(series, category);
+        displayPagination(data.total_results, category);
+    }
+
+    function displaySeries(series, category) {
+        
+        const seriesContainerId = `series-container-${category}`;
+        const seriesContainer = document.getElementById(seriesContainerId);
+        seriesContainer.innerHTML = '';
+  
+        const startIndex = (currentSeriesPage - 1) * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+        const seriesToDisplay = series.slice(startIndex, endIndex);
+  
+        seriesToDisplay.forEach(serie => {
+            const card = createCard(serie);
+            seriesContainer.appendChild(card);
+        });
+    }
+    function createCard(serie) {
+
+        const container = document.getElementById('container')
+        
+        const card = document.createElement('div');
+        card.classList.add('card', 'm-2'); 
+        card.style.height = '500px';
+        const image = document.createElement('img');
+        image.classList.add('card-img-top');
+        image.classList.add('serie-image'); // Dodajemy klasę 'serie-image' tutaj
+        image.src = 'https://image.tmdb.org/t/p/w500' + serie.poster_path;
+        image.alt = serie.name;
+        image.style.height = '400px';
+
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+
+        const title = document.createElement('h5');
+        title.classList.add('card-title');
+        title.textContent = serie.name;
+
+        const rating = document.createElement('p');
+        rating.classList.add('card-text');
+        rating.textContent = 'Note: ' + serie.vote_average.toFixed(1);
+
+        cardBody.appendChild(title);
+        card.appendChild(image);
+        card.appendChild(cardBody);
+        cardBody.appendChild(rating);
+
+        return card;
+    }
+
+    function displayPagination(totalItems, category) {
+        const totalPages = Math.ceil(totalItems / itemsPerPage);
+        const paginationContainerId = `series-pagination-${category}`;
+        const paginationContainer = document.getElementById(paginationContainerId);
+        paginationContainer.innerHTML = '';
+  
+        const paginationList = document.createElement('ul');
+        paginationList.classList.add('pagination');
+  
+        const numPagesToShow = Math.min(4, totalPages);
+  
+        for (let i = 1; i <= numPagesToShow; i++) {
+            const pageButton = createPaginationButton(i, category);
+            paginationList.appendChild(pageButton);
+        }
+  
+        paginationContainer.appendChild(paginationList);
+    }
+    
+
+    function createPaginationButton(label, category) {
+        const listItem = document.createElement('li');
+        listItem.classList.add('page-item');
+  
+        const link = document.createElement('a');
+        link.classList.add('page-link');
+        link.href = '#';
+        link.textContent = label;
+  
+        link.addEventListener('click', async function(event) {
+            event.preventDefault();
+            currentSeriesPage = label;
+            await fetchPage(category, currentSeriesPage);
+        });
+  
+        listItem.appendChild(link);
+        return listItem;
+    }
+
+    await fetchPage('comedy', currentSeriesPage); // Dodane wywołanie dla kategorii komedii
+    await fetchPage('action_adventure', currentSeriesPage); // Dodane wywołanie dla kategorii akcji i przygody
+    await fetchPage('crime', currentSeriesPage); // Dodane wywołanie dla kategorii kryminał
+    await fetchPage('family', currentSeriesPage); // Dodane wywołanie dla kategorii rodziny
+});
             
-                function processContent(data) {
-                    const totalItems = data.length; // Zamiast używać total_results, korzystamy z długości złączonej listy wyników
-            
-                    displayItems(data, 'tv-series-list-popular', currentSeriesPage, itemsPerPage);
-                    displayPagination(totalItems, 'series-pagination', currentSeriesPage);
-                }
-            
-                function displayItems(items, containerId, currentPage, itemsPerPage) {
-                    const container = document.getElementById(containerId);
-                    container.innerHTML = '';
-            
-                    const startIndex = (currentPage - 1) * itemsPerPage;
-                    const endIndex = Math.min(startIndex + itemsPerPage, items.length);
-            
-                    const itemsToDisplay = items.slice(startIndex, endIndex);
-            
-                    itemsToDisplay.forEach(item => {
-                        const col = document.createElement('div');
-                        col.classList.add('col');
-            
-                        const card = document.createElement('div');
-                        card.classList.add('card', 'h-100');
-            
-                        const image = document.createElement('img');
-                        image.classList.add('card-img-top');
-                        image.src = 'https://image.tmdb.org/t/p/w500' + item.poster_path;
-                        image.alt = item.title || item.name;
-                        card.appendChild(image);
-            
-                        const cardBody = document.createElement('div');
-                        cardBody.classList.add('cardBody');
-            
-                        const title = document.createElement('p');
-                        title.classList.add('card-title');
-                        title.textContent = item.title || item.name; // Użyj tytułu filmu lub serialu
-                        cardBody.appendChild(title);
-            
-                        card.appendChild(cardBody);
-                        col.appendChild(card);
-                        container.appendChild(col);
-                    });
-                }
-            
-                function displayPagination(totalItems, containerId, currentPage) {
-                    const totalPages = Math.ceil(totalItems / itemsPerPage);
-                    const paginationContainer = document.getElementById(containerId);
-                    paginationContainer.innerHTML = '';
-            
-                    const paginationList = document.createElement('ul');
-                    paginationList.classList.add('pagination');
-            
-                    const prevButton = createPaginationButton('précedent', currentPage - 1, currentPage === 1);
-                    paginationList.appendChild(prevButton);
-            
-                    for (let i = 1; i <= totalPages; i++) {
-                        const pageButton = createPaginationButton(i, i, false);
-                        paginationList.appendChild(pageButton);
-                    }
-            
-                    const nextButton = createPaginationButton('suivante', currentPage + 1, currentPage === totalPages);
-                    paginationList.appendChild(nextButton);
-            
-                    paginationContainer.appendChild(paginationList);
-                }
-            
-                function createPaginationButton(label, page, isDisabled) {
-                    const listItem = document.createElement('li');
-                    listItem.classList.add('page-item');
-            
-                    const link = document.createElement('a');
-                    link.classList.add('page-link');
-                    link.href = '#';
-                    link.textContent = label;
-                    if (isDisabled) {
-                        listItem.classList.add('disabled');
-                        link.setAttribute('aria-disabled', 'true');
-                    } else {
-                        link.addEventListener('click', function() {
-                            currentSeriesPage = page;
-                            fetchProcessContent(page);
-                        });
-                    }
-            
-                    listItem.appendChild(link);
-                    return listItem;
-                }
-            
-                // Wywołanie funkcji pobierającej treść
-                fetchProcessContent(currentSeriesPage);
-            });
-                     
